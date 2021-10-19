e695feb5b2339f8719ad727affabef9ff97bb2dd
Utsav <utsaga22@gmail.com>
2021-10-19 22:08:40 +0530

    Tic Tac Toe Game by Utsav

---
+++ ext.js
@@ -0,0 +1,60 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("beep-06.mp3")
+let gameover = new Audio("mixkit-wrong-answer-fail-notification-946.wav")
+let turn = "X"
+let isgameover = false;
+
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
+}
+
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
+            document.querySelector('#det').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true;
+            gameover.play();
+            document.querySelector('.image').getElementsByTagName('img')[0].style.width = "150px";
+            document.querySelector('.image').getElementsByTagName('img')[0].style.height = "100px";
+        }
+    })
+}
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover) {
+                document.getElementById("det").innerText = "Turn for " + turn;
+            }
+        }
+    })
+})
+
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    isgameover = false
+    document.getElementsByClassName("det")[0].innerText = "Turn for " + turn;
+    document.querySelector('.image').getElementsByTagName('img')[0].style.width = "0px";
+    document.querySelector('.image').getElementsByTagName('img')[0].style.height = "0px";
+})
